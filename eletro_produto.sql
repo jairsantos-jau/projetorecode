5555771335ff332b1aefeea6ced98f3a11ac71bd
jairsantos-jau <43978530+jairsantos-jau@users.noreply.github.com>
2020-10-26 03:49:07 -0300

    Update eletro_produto.sql

--- eletro_produto.sql
+++ eletro_produto.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `eletro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `eletro`;
 -- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
 --
 -- Host: localhost    Database: eletro
@@ -31,7 +33,7 @@ CREATE TABLE `produto` (
   `imagem` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`idproduto`),
   UNIQUE KEY `imagem_UNIQUE` (`imagem`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +42,7 @@ CREATE TABLE `produto` (
 
 LOCK TABLES `produto` WRITE;
 /*!40000 ALTER TABLE `produto` DISABLE KEYS */;
-INSERT INTO `produto` VALUES (1,'geladeira','geladeira tipo A',6.10,5.10,NULL),(2,'guarda roupa','guarda roupa tipo A',2.39,1.02,NULL),(3,'macbook','tmacbook tipo A',6.39,5.02,NULL),(4,'fogao','fogao tipo A',6.39,5.02,NULL);
+INSERT INTO `produto` VALUES (1,'geladeira','geladeira tipo A',6.10,5.10,'image/refrigerador.png'),(2,'guarda roupa','guarda roupa tipo A',2.39,1.02,'image/guardaroupa.png'),(3,'macbook','tmacbook tipo A',6.39,5.02,'image/notebook.png'),(4,'fogao','fogao tipo A',6.39,5.02,'image/fogao.png'),(5,'celular xiaomi','nova geracao',2000.00,1900.00,'image/xiaomi.png'),(6,'iphone','tipo xp visual',6890.00,5890.00,'image/iphone.png'),(7,'iphonexp','cameras ultra',3080.00,2165.00,'image/iphonexp.png'),(8,'smartphone Xiomi','funcionalidade extra',1999.00,1444.00,'image/smartphonexiomi.png'),(9,'smart G','camera tripa',3218.00,2218.00,'image/smartg.png'),(10,'livro java','linguagem didadtica',55.00,43.00,'image/livrojava.png');
 /*!40000 ALTER TABLE `produto` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-23 19:49:13
+-- Dump completed on 2020-10-26  3:44:47
